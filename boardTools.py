cef6f132b9ab7ddcc00a61a0bff6e07f69e8d4bf
hkniner <henry.koch@maine.edu>
2024-12-03 10:17:39 -0500

    created main() function + bugfix
    -main() in main.py created
    -Board bugfix

--- boardTools.py
+++ boardTools.py
@@ -26,31 +26,47 @@ def printBoard(world):
     for row in range(len(board)):
 
         for col in range(len(board)):
+            #Printing player
             if world["playerLoc"]["x"] == col and \
                 world["playerLoc"]["y"] == row:
                 #Print an X in the cell
                 coloredX = setColor(world["player"]["color"], "X")
-                outputString += f"[{coloredX :>2}]"
+                outputString += f"[{coloredX:>2}"
+                outputString += f"{" ":>1}]"
+            
+            #Printing Front Door POI
             elif world["POI"]["Front Door"]["x"] == col and \
                 world["POI"]["Front Door"]["y"] == row:
                 #Print an F in the cell
                 coloredF = setColor("purple", "F")
-                outputString += f"[{coloredF :>2}]"
+                outputString += f"[{coloredF:>2}"
+                outputString += f"{" ":>1}]"
+            
+            #Printing Parents Room POI
             elif world["POI"]["Parents Room"]["x"] == col and \
                 world["POI"]["Parents Room"]["y"] == row:
                 #Print an P in the cell
                 coloredP = setColor("purple", "P")
-                outputString += f"[{coloredP :>2}]"
+                outputString += f"[{coloredP:>2}"
+                outputString += f"{" ":>1}]"
+            
+            #Printing Your Room POI
             elif world["POI"]["Your Room"]["x"] == col and \
                 world["POI"]["Your Room"]["y"] == row:
                 #Print an R in the cell
                 coloredR = setColor("purple", "R")
-                outputString += f"[{coloredR :>2}]"
+                outputString += f"[{coloredR:>2}"
+                outputString += f"{" ":>1}]"
+            
+            #Printing Kitchen POI
             elif world["POI"]["Kitchen"]["x"] == col and \
                 world["POI"]["Kitchen"]["y"] == row:
                 #Print an K in the cell
                 coloredK = setColor("purple", "K")
-                outputString += f"[{coloredK :>2}]"
+                outputString += f"[{coloredK:>2}"
+                outputString += f"{" ":>1}]"
+
+            #Printing Empty Spaces
             elif board[row][col] == 0:
                 outputString += f"[{" ":2}]"
         outputString += "\n"
--- main.py
+++ main.py
@@ -7,33 +7,43 @@ Dec 2, 2024
 from boardTools import *
 from gameTools import *
 from inputProcess import *
+import time
 
-# Creating world and location of player
-world = {}
-loc = {
-    "x":3,
-    "y":3
-}
-world["board"] = createBoard(7)
-world["playerLoc"] = loc
-world["player"] = createPlayer()
+def main():
+    # Creating world and location of player
+    world = {}
+    loc = {
+        "x":3,
+        "y":3
+    }
+    world["board"] = createBoard(7)
+    world["playerLoc"] = loc
+    world["player"] = createPlayer()
 
-#creating pois and locations
-world["POI"] = {}
-world["POI"]["Front Door"] = {
-    "x":3,
-    "y":0
-}
-world["POI"]["Parents Room"] = {
-    "x":6,
-    "y":3
-}
-world["POI"]["Your Room"] = {
-    "x":3,
-    "y":6
-}
-world["POI"]["Kitchen"] = {
-    "x":0,
-    "y":3
-}
-printBoard(world)
+    #creating pois and locations
+    world["POI"] = {}
+    world["POI"]["Front Door"] = {
+        "x":3,
+        "y":0
+    }
+    world["POI"]["Parents Room"] = {
+        "x":6,
+        "y":3
+    }
+    world["POI"]["Your Room"] = {
+        "x":3,
+        "y":6
+    }
+    world["POI"]["Kitchen"] = {
+        "x":0,
+        "y":3
+    }
+    printBoard(world)
+
+    #Starting gameplay loop
+
+    while True:
+        print("testing")
+        break
+
+main()
